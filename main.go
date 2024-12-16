9f4b470fd6ff89dce40ccc22060c91c7d1c125aa
isaacgoncod <isaacgoncod@gmail.com>
2024-12-16 13:45:47 -0300

    add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/isaacgoncod/go-project-example/router"
+import (
+	"github.com/isaacgoncod/go-project-example/config"
+	"github.com/isaacgoncod/go-project-example/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
-	router.Initialize()
+	logger = *config.GetLogger("main")
 
+	err := config.Init()
+
+	if err != nil {
+		logger.Errf("Config Initialization Error: %v", err)
+
+		return
+	}
+
+	router.Initialize()
 }
